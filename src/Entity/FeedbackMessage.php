4b5d815b71b5b5b35dbb0fa282c86f96d37d9bf0
Ilya Donskikh <gesar.93@mail.ru>
2026-02-12 07:26:15 +0300

    Создать сущности User и FeedbackMessage

---
+++ src/Entity/FeedbackMessage.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FeedbackMessageRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FeedbackMessageRepository::class)]
+class FeedbackMessage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    #[ORM\ManyToOne(inversedBy: 'feedbackMessages')]
+    private ?User $user = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Ignore;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'])]
+#[ORM\Table(name: 'users')]
+class User implements PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $phone = null;
+
+    #[Ignore]
+    #[ORM\Column(name: 'password', length: 255)]
+    private string $passwordHash;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $createdAt = null;
+
+    /**
+     * @var Collection<int, FeedbackMessage>
+     */
+    #[ORM\OneToMany(targetEntity: FeedbackMessage::class, mappedBy: 'user')]
+    private Collection $feedbackMessages;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+        $this->feedbackMessages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash(string $passwordHash): static
+    {
+        $this->passwordHash = $passwordHash;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, FeedbackMessage>
+     */
+    public function getFeedbackMessages(): Collection
+    {
+        return $this->feedbackMessages;
+    }
+
+    public function addFeedbackMessage(FeedbackMessage $feedbackMessage): static
+    {
+        if (!$this->feedbackMessages->contains($feedbackMessage)) {
+            $this->feedbackMessages->add($feedbackMessage);
+            $feedbackMessage->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFeedbackMessage(FeedbackMessage $feedbackMessage): static
+    {
+        if ($this->feedbackMessages->removeElement($feedbackMessage)) {
+            if ($feedbackMessage->getUser() === $this) {
+                $feedbackMessage->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
